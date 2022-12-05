7d96581937bbdf5b10f894e52e78ff8b18401605
Wiktor Kacprzak <ceb.zabal5@gmail.com>
2022-12-05 22:26:21 +0100

    Seller with methods

--- seller.py
+++ seller.py
@@ -1,19 +1,25 @@
-from typing import List, Tuple, Dict
+from typing import List, Tuple, Dict, Union, Function
+from delivery_functionality import Delivery
+from discount_functionality import Discount
+
 
 class Seller:
-    def __init__(self, id: int, store: Dict[int:Tuple[float, int]]):
+    def __init__(self, id: int, store: Dict[int:Tuple[float, int]], delivery_info, discount_info):
         self.id = id
         self.store = store
+        self.delivery = Delivery(delivery_info)
+        self.discount = Discount(discount_info)
 
-    #funkcja przesylki
-    #funkcja znizek
-
-    def get_delivery_price(self):
-
-        return
+    def get_delivery_price(self, weight: int):
+        return self.delivery.get_cost(weight)
 
     def get_discounted_price(self):
-        return
+        prices = []
+        amount = 0
+        for k, v in self.get_products().items():
+            prices.append(v[0])
+            amount += v[1]
+        return self.discount.get_new_cost(prices, amount)
 
     def get_products(self):  #zwraca asortyment sprzedawcy
         return self.store
